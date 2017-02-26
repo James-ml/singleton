f6c1318611da89d4cfdfbac68f1ece2e26df55eb
Jims <1044416771@qq.com>
2017-02-26 13:37:56 +0800

    新增了注释

--- src/demo/Singleton04.java
+++ src/demo/Singleton04.java
@@ -8,6 +8,9 @@ public class Singleton04 {
     private Singleton04(){
         System.out.println("Singleton04 is create");
     }
+    //public的作用是允许外部直接访问该变量
+    //static的作用是让外部可以使用 类名.变量名 的方式来访问变量
+    //final的作用是禁止外部对该变量进行修改
     public static final Singleton04 instance = new Singleton04();
 }
 class Test04{
